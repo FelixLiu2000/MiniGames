03d5f6adcaabc9a26c9148284892e73ceed35e9d
Diego Nunez <diego.nunez@mail.utoronto.ca>
2019-10-23 20:52:54 -0400

    Mock Home Page with onClick function and @strings in strings.xml

--- phase1/Game/app/src/main/java/com/example/game/MainActivity.java
+++ phase1/Game/app/src/main/java/com/example/game/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View view) {
+        // Do something in response to button
+        System.out.println("This would start the game at game corresponding to progress.");
     }
 }
