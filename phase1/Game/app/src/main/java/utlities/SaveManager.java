e7fcaaede44f129f976388834634e2fe285fb34c
Eric Zhengyu Zhu <e.zhu@mail.utoronto.com>
2019-10-25 15:56:48 -0400

    Player and SaveManager are done. All classes that are to be saved need to implement Serializeable

---
+++ phase1/Game/app/src/main/java/utlities/SaveManager.java
@@ -0,0 +1,72 @@
+package utlities;
+
+
+import android.os.Environment;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+
+/**
+ * This class provides static methods for saving and loading, i.e., do not create an instance of this class
+ *
+ * Call loadPlayer with the player's name to load a player
+ * All of your classes must implement Seralizeable to be saved
+ */
+public class SaveManager {
+    final static String filePath = Environment.getExternalStorageDirectory() + "/"+"saves"+"/";
+
+    /**
+     * Saves a player.
+     * @param player
+     * @return true if properly saved else false
+     */
+    public static boolean save(Player player){
+        File saveFolder = new File(filePath);
+        if(!saveFolder.exists()) {
+            saveFolder.mkdirs();
+        }
+
+        System.out.println(saveFolder.exists());
+
+        File saveFile = new File(filePath + player.getName() + ".sav");
+
+        FileOutputStream fileOutputStream;
+        ObjectOutputStream objectOutputStream;
+        try {
+            saveFile.createNewFile();
+            fileOutputStream = new FileOutputStream(saveFile);
+            objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(player);
+            objectOutputStream.close();
+        }catch(Exception e){
+            System.out.println("Failed with exception: " + e);
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * This method will load a player, provided their username
+     * @param name
+     * @return
+     */
+    public static Player loadPlayer(String name){
+        FileInputStream fileInputStream;
+        ObjectInputStream objectInputStream;
+        Player player = null;
+        try{
+            fileInputStream = new FileInputStream(filePath + name + ".sav");
+            objectInputStream = new ObjectInputStream(fileInputStream);
+            player = (Player)objectInputStream.readObject();
+            objectInputStream.close();
+        }catch(Exception e) {
+            System.out.println("Failed with exception: " + e);
+        }
+        return player;
+    }
+
+}
