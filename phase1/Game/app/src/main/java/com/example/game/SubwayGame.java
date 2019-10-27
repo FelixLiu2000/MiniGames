3012bc6eadd8536e6648c0753f1d24b57773b7f1
Emily Mazor <emily.mazor@mail.utoronto.ca>
2019-10-27 14:33:11 -0400

    Initial commit to subway branch

---
+++ phase1/Game/app/src/main/java/com/example/game/SubwayGame.java
@@ -0,0 +1,80 @@
+package com.example.game;
+
+import java.util.ArrayList;
+import java.util.Timer;
+
+import utilities.Game;
+
+public class SubwayGame extends Game {
+    private ArrayList<SubwayObstacle> obstacles = new ArrayList<>();
+    private int score;
+
+    SubwayGame() {
+        super();
+        this.score = 10;
+
+    }
+
+    private void createRunner() {
+        SubwayRunner runner = new SubwayRunner();
+    }
+
+    private int play() {
+        // play until time=0
+        // create obstacle every 3 seconds
+        createObstacle();
+        // check if runner and an obstacle are in the same position every second
+        checkCollision();
+
+        return score;
+    }
+
+    private void createObstacle() {
+        int obstacleLane = pickLane();
+        SubwayObstacle newObstacle = new SubwayObstacle(obstacleLane);
+        obstacles.add(newObstacle);
+    }
+
+    // determine which lane the new obstacle is in
+    private int pickLane() {
+        int lane;
+        double random = Math.random();
+        if (random < 0.33) {
+            lane = 1;
+        } else if (random < 0.66) {
+            lane = 2;
+        } else lane = 3;
+
+        return lane;
+    }
+
+    // check if runner and obstacle are in the same position
+    private void checkCollision() {
+        // if position of runner and obstacle is the same
+            // decrease score
+    }
+
+    private void decreaseScore() {
+        this.score -= 1;
+    }
+
+
+//    private void moveObstacle() {
+//
+//    }
+//
+//    private void moveRunner() {
+//        // move right
+//
+//        // move left
+//
+//    }
+//
+//    // check if runner has hit an obstacle
+//    private void checkObstacle() {
+//        // if runner has same position as obstacle
+//            // score -= 1
+//
+//    }
+
+}
---
+++ phase1/Game/app/src/main/java/com/example/game/SubwayGameActivity.java
@@ -0,0 +1,38 @@
+package com.example.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+public class SubwayGameActivity extends AppCompatActivity {
+    private ArrayList<ImageView> obstacles = new ArrayList<>();
+    private ImageView runner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subway_game);
+    }
+
+    // move runner right when right button is clicked
+    private void moveRight(View view) {
+
+    }
+
+    // move runner left when left button is clicked
+    private void moveLeft() {
+
+    }
+
+    // move all obstacles down every second
+    private void moveDown() {
+        for (int i=0; i<obstacles.size(); i++ ) {
+            ImageView obstacle = obstacles.get(i);
+            // move down
+        }
+    }
+}
