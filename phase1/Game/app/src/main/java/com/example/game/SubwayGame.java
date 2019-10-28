9438ad86a64bde912b844ad8ed623423afd16bef
Emily Mazor <emily.mazor@mail.utoronto.ca>
2019-10-28 16:13:13 -0400

    Updates to subway game

--- phase1/Game/app/src/main/java/com/example/game/SubwayGame.java
+++ phase1/Game/app/src/main/java/com/example/game/SubwayGame.java
@@ -1,80 +1,112 @@
 package com.example.game;
 
-import java.util.ArrayList;
-import java.util.Timer;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.ImageView;
 
 import utilities.Game;
 
 public class SubwayGame extends Game {
-    private ArrayList<SubwayObstacle> obstacles = new ArrayList<>();
+    private SubwayGameActivity activity;
+//    private ArrayList<SubwayObstacle> obstacles = new ArrayList<>();
     private int score;
 
-    SubwayGame() {
+    SubwayGame(SubwayGameActivity activity) {
         super();
         this.score = 10;
+        this.activity = activity;
 
     }
 
-    private void createRunner() {
-        SubwayRunner runner = new SubwayRunner();
-    }
+//    private void startRound() {
+//        SubwayRunner runner = new SubwayRunner();
+//    }
 
     private int play() {
-        // play until time=0
-        // create obstacle every 3 seconds
-        createObstacle();
-        // check if runner and an obstacle are in the same position every second
-        checkCollision();
-
+//        startRound();
+        // create 60 second timer
+        CountDownTimer timer = new CountDownTimer(60000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                // check for collisions every second
+                checkCollision();
+                // move all obstacles down every second
+                activity.moveDown();
+                // create new obstacle every 3 seconds
+                if (millisUntilFinished % 3000 == 0) {
+                    createObstacle();
+                }
+            }
+            @Override
+            public void onFinish() {
+                System.out.println("Game Over!");
+            }
+        };
+        timer.start();
         return score;
     }
 
+    /** create a new obstacle and add it to obstacles ArrayList in SubwayGameActivity */
     private void createObstacle() {
+        ImageView newObstacle = new ImageView(activity);
+        // set obstacle position
+        setPosition(newObstacle);
+//        SubwayObstacle newObstacle = new SubwayObstacle(obstacleLane);
+//        obstacles.add(newObstacle);
+    }
+
+    /** Set location of the new obstacle */
+    private void setPosition(ImageView newObstacle) {
+        // automatically set y variable to 0
+        newObstacle.setY(0);
+        // randomly pick a lane
         int obstacleLane = pickLane();
-        SubwayObstacle newObstacle = new SubwayObstacle(obstacleLane);
-        obstacles.add(newObstacle);
+        // set obstacle's x position based on the lane
+        if (obstacleLane == 1) {
+            newObstacle.setX(16);
+        } else if (obstacleLane == 2) {
+            newObstacle.setX(204);
+        } else newObstacle.setX(340); // lane 3
     }
 
-    // determine which lane the new obstacle is in
+
+    /** randomly determine which lane the new obstacle is in */
     private int pickLane() {
-        int lane;
-        double random = Math.random();
-        if (random < 0.33) {
-            lane = 1;
-        } else if (random < 0.66) {
-            lane = 2;
-        } else lane = 3;
+        return (int)(Math.random()*4);
 
-        return lane;
+//        int lane;
+//        double random = Math.random();
+//        if (random < 0.33) {
+//            lane = 1;
+//        } else if (random < 0.66) {
+//            lane = 2;
+//        } else lane = 3;
+//
+//        return lane;
     }
 
-    // check if runner and obstacle are in the same position
+    /** check if runner and obstacle are in the same position and decrease score by 1 if they are */
     private void checkCollision() {
-        // if position of runner and obstacle is the same
-            // decrease score
+        // get position of runner
+        View runner = activity.findViewById(R.id.subwayRunner);
+        float runnerX = runner.getX();
+        float runnerY = runner.getY();
+        // loop through obstacles
+        for (int i=0; i<activity.obstacles.size(); i++) {
+            // get position of obstacle
+            View obstacle = activity.obstacles.get(i);
+            float obstacleX = obstacle.getX();
+            float obstacleY = obstacle.getY();
+            // if position of runner and obstacle is the same
+            if (obstacleX == runnerX && obstacleY == runnerY) {
+                // decrease score
+                decreaseScore();
+            }
+        }
     }
 
+    /** decrease the score by 1 */
     private void decreaseScore() {
         this.score -= 1;
     }
-
-
-//    private void moveObstacle() {
-//
-//    }
-//
-//    private void moveRunner() {
-//        // move right
-//
-//        // move left
-//
-//    }
-//
-//    // check if runner has hit an obstacle
-//    private void checkObstacle() {
-//        // if runner has same position as obstacle
-//            // score -= 1
-//
-//    }
-
 }
--- phase1/Game/app/src/main/java/com/example/game/SubwayGameActivity.java
+++ phase1/Game/app/src/main/java/com/example/game/SubwayGameActivity.java
@@ -1,38 +1,72 @@
 package com.example.game;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
 public class SubwayGameActivity extends AppCompatActivity {
-    private ArrayList<ImageView> obstacles = new ArrayList<>();
+    ArrayList<ImageView> obstacles = new ArrayList<>();
     private ImageView runner;
+    private SubwayGame game;
+    // runner's x and y coordinates
+    private float runnerX;
+    private float runnerY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subway_game);
+        runnerX = runner.getX();
+        runnerY = runner.getY();
     }
 
-    // move runner right when right button is clicked
-    private void moveRight(View view) {
+
+    /** move runner right when right button is clicked */
+    public void moveRight(View view) {
+//        float yPosition = view.getY();
+//        float xPosition = view.getX();
+        // if runner is in lane 1
+        if (runnerX == 16) {
+            // move to lane 2
+            TranslateAnimation runnerRight = new TranslateAnimation(runnerX, 204, runnerY, runnerY);
+        // if runner is in lane 2
+        } else if (runnerX == 204) {
+            // move to lane 3
+            TranslateAnimation runnerRight = new TranslateAnimation(runnerX, 340, runnerY, runnerY);
+        }
 
     }
 
-    // move runner left when left button is clicked
-    private void moveLeft() {
-
+    /** move runner left when left button is clicked */
+    public void moveLeft(View view) {
+//        float yPosition = view.getY();
+//        float xPosition = view.getX();
+        // if runner is in lane 3
+        if (runnerX == 340) {
+            // move to lane 2
+            TranslateAnimation runnerLeft = new TranslateAnimation(runnerX, 204, runnerY, runnerY);
+        // if  runner is in lane 2
+        } else if (runnerX == 204) {
+            // move to lane 1
+            TranslateAnimation runnerLeft = new TranslateAnimation(runnerX, 16, runnerY, runnerY);
+        }
     }
 
-    // move all obstacles down every second
-    private void moveDown() {
-        for (int i=0; i<obstacles.size(); i++ ) {
+    /** move all obstacles down every second */
+    public void moveDown() {
+        // loop through obstacles
+        for (int i=0; i<obstacles.size(); i++) {
             ImageView obstacle = obstacles.get(i);
             // move down
+            TranslateAnimation obstacleDown = new TranslateAnimation(obstacle.getX(),
+                    obstacle.getX(),
+                    obstacle.getY(),
+                    obstacle.getY()+1);
         }
     }
 }
