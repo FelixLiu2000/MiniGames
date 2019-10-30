5602989baac4a6961accc3e42f585a129aa01f6e
alexandrawaddell <alex.waddell@mail.utoronto.ca>
2019-10-30 17:03:50 -0400

    Simplified code and added comments. Also resolved issue when updating score.

--- phase1/Game/app/src/main/java/com/example/game/activities/CardGameActivity.java
+++ phase1/Game/app/src/main/java/com/example/game/activities/CardGameActivity.java
@@ -19,26 +19,31 @@ public class CardGameActivity extends AppCompatActivity {
   private ArrayList<ImageView> buttons = new ArrayList<>();
   protected TextView score;
   private ImageView card_view;
-  private Integer[] cardsArray = {101, 102, 103, 104, 105, 106, 201, 202, 203, 204, 205, 206};
+  // Initializes an array of integers that will refer to the images to be compared. The
+  // cards that end in the same numbers will be considered matches (ex. 11 matches 21).
+  private Integer[] cardsArray = {11, 12, 13, 14, 15, 16, 21, 22, 23, 24, 25, 26};
   int firstCard, secondCard;
   int clickedFirst, clickedSecond;
   int cardNum = 1;
   CardGame cardGame = new CardGame(60);
   TextView time;
   int stat = 0;
+  int cardsLeft = 12;
 
-  public CardGameActivity() {
-    Collections.shuffle(Arrays.asList(cardsArray));
-  }
+//  public CardGameActivity() {
+//    Collections.shuffle(Arrays.asList(cardsArray));
+//  }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    Collections.shuffle(Arrays.asList(cardsArray));
     setContentView(R.layout.activity_card_game);
-    score = findViewById(R.id.scoreValueText);
+    score = findViewById(R.id.score);
     time = findViewById(R.id.time);
     time.setText(String.valueOf(0));
 
+    // Adds all the images to an array so they can be iterated through.
     buttons.add((ImageView) findViewById(R.id.card_11));
     buttons.add((ImageView) findViewById(R.id.card_12));
     buttons.add((ImageView) findViewById(R.id.card_13));
@@ -82,110 +87,89 @@ public class CardGameActivity extends AppCompatActivity {
   }
 
   public void flip(int card, ImageView iv) {
-    // return other side of the card
-    if (cardsArray[card] == 101 || cardsArray[card] == 201) {
+    // Set the image resource for each button, so the item to be compared. This is how the
+    // numbers in the array (100 vs. 200) each get connected to the same image.
+    if (cardsArray[card] == 11 || cardsArray[card] == 21) {
       iv.setImageResource(R.drawable.orange_circle);
-    } else if (cardsArray[card] == 102 || cardsArray[card] == 202){
+    } else if (cardsArray[card] == 12 || cardsArray[card] == 22){
       iv.setImageResource(R.drawable.pink_ring);
-    } else if (cardsArray[card] == 103 || cardsArray[card] == 203){
+    } else if (cardsArray[card] == 13 || cardsArray[card] == 23){
       iv.setImageResource(R.drawable.green_ring);
-    } else if (cardsArray[card] == 104 || cardsArray[card] == 204){
+    } else if (cardsArray[card] == 14 || cardsArray[card] == 24){
       iv.setImageResource(R.drawable.red_circle);
-    } else if (cardsArray[card] == 105 || cardsArray[card] == 205){
+    } else if (cardsArray[card] == 15 || cardsArray[card] == 25){
       iv.setImageResource(R.drawable.yellow_square);
-    } else if (cardsArray[card] == 106 || cardsArray[card] == 206){
+    } else if (cardsArray[card] == 16 || cardsArray[card] == 26){
       iv.setImageResource(R.drawable.blue_square);
     }
 
-    // checks which card has been selected to send to back end
+    // checks which card has been selected and sets to temporary variables, to be used in Activity
+    // or sent to back end.
     if(cardNum == 1){
       firstCard = cardsArray[card];
-      if(firstCard > 200){
-        firstCard = firstCard - 100;
+      if(firstCard > 20){
+        firstCard = firstCard - 10;
       }
+      // updates number of card being checked to look for second card.
       cardNum = 2;
       clickedFirst = card;
 
+      // Sets the card that was just flipped so that it cannot be clicked again.
       iv.setEnabled(false);
     } else if (cardNum == 2) {
       secondCard = cardsArray[card];
-      if (secondCard > 200) {
-        secondCard = secondCard - 100;
+      if (secondCard > 20) {
+        secondCard = secondCard - 10;
       }
+      // updates number of card so that it goes back to looking for first card.
       cardNum = 1;
       clickedSecond = card;
 
+      // After two cards have been clicked, set all the cards so they cannot be clicked again,
+      // this would be cheating in a memory game.
       for (int i = 0; i < buttons.size(); i++) {
         buttons.get(i).setEnabled(false);
       }
-      Handler handler = new Handler();
+      // This puts a delay the call to update (which either deletes or flips back the
+      // cards so that, the player can see which cards have been flipped and try and
+      // remember the cards.
+      final Handler handler = new Handler();
       handler.postDelayed(new Runnable(){
           @Override
           public void run(){
-              //check if the selected images are equal
+            // Checks if images are a match and either flips cards back over or removes them
+            // from the board.
               update();
           }
           }, 1000);
-      //update();
       }
   }
   public void update(){
     if(cardGame.check(firstCard, secondCard)){
-      if(clickedFirst == 0){
-        findViewById(R.id.card_11).setVisibility(View.INVISIBLE);
-      } else if (clickedFirst == 1) {
-        findViewById(R.id.card_12).setVisibility(View.INVISIBLE);
-      } else if (clickedFirst == 2) {
-        findViewById(R.id.card_13).setVisibility(View.INVISIBLE);
-      } else if (clickedFirst == 3) {
-        findViewById(R.id.card_14).setVisibility(View.INVISIBLE);
-      } else if (clickedFirst == 4) {
-        findViewById(R.id.card_21).setVisibility(View.INVISIBLE);
-      } else if (clickedFirst == 5) {
-        findViewById(R.id.card_22).setVisibility(View.INVISIBLE);
-      } else if (clickedFirst == 6) {
-        findViewById(R.id.card_23).setVisibility(View.INVISIBLE);
-      } else if (clickedFirst == 7) {
-        findViewById(R.id.card_24).setVisibility(View.INVISIBLE);
-      } else if (clickedFirst == 8) {
-        findViewById(R.id.card_31).setVisibility(View.INVISIBLE);
-      } else if (clickedFirst == 9) {
-        findViewById(R.id.card_32).setVisibility(View.INVISIBLE);
-      } else if (clickedFirst == 10) {
-        findViewById(R.id.card_33).setVisibility(View.INVISIBLE);
-      } else if (clickedFirst == 11) {
-        findViewById(R.id.card_34).setVisibility(View.INVISIBLE);
+      // If the two cards are equal (checked on back-end) set the first card/button to invisible.
+      for (int i = 0; i < buttons.size(); i++) {
+        if (clickedFirst == i) {
+          buttons.get(i).setVisibility(View.INVISIBLE);
+        }
       }
-      if(clickedSecond == 0){
-        findViewById(R.id.card_11).setVisibility(View.INVISIBLE);
-      } else if (clickedSecond == 1) {
-        findViewById(R.id.card_12).setVisibility(View.INVISIBLE);
-      } else if (clickedSecond == 2) {
-        findViewById(R.id.card_13).setVisibility(View.INVISIBLE);
-      } else if (clickedSecond == 3) {
-        findViewById(R.id.card_14).setVisibility(View.INVISIBLE);
-      } else if (clickedSecond == 4) {
-        findViewById(R.id.card_21).setVisibility(View.INVISIBLE);
-      } else if (clickedSecond == 5) {
-        findViewById(R.id.card_22).setVisibility(View.INVISIBLE);
-      } else if (clickedSecond == 6) {
-        findViewById(R.id.card_23).setVisibility(View.INVISIBLE);
-      } else if (clickedSecond == 7) {
-        findViewById(R.id.card_24).setVisibility(View.INVISIBLE);
-      } else if (clickedSecond == 8) {
-        findViewById(R.id.card_31).setVisibility(View.INVISIBLE);
-      } else if (clickedSecond == 9) {
-        findViewById(R.id.card_32).setVisibility(View.INVISIBLE);
-      } else if (clickedSecond == 10) {
-        findViewById(R.id.card_33).setVisibility(View.INVISIBLE);
-      } else if (clickedSecond == 11) {
-        findViewById(R.id.card_34).setVisibility(View.INVISIBLE);
+      // If the two cards are equal, set the second card/button to invisible.
+      for (int i = 0; i < buttons.size(); i++) {
+        if (clickedSecond == i) {
+          buttons.get(i).setVisibility(View.INVISIBLE);
+        }
+      }
+      stat += 1;
+      cardsLeft -= 2;
+      String updatedScore = "Score: " + stat;
+      score.setText(updatedScore);
+      if (cardsLeft == 0){
+        // TODO : reshuffle and make visible
       }
     } else {
+      // If the two cards are not the same, set the image resource back to the green square
+      // (back of card), and enable the card so that it can be clicked again.
       for (int i = 0; i < buttons.size(); i++) {
         buttons.get(i).setImageResource(R.drawable.memory_card);
-        stat += 1;
-        score.setText(String.valueOf(stat));
       }
     }
       for (int i = 0; i < buttons.size(); i++) {
