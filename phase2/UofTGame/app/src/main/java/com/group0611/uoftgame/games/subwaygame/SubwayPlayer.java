9d8f06e040dedd4f431377622039ab3aedf37b15
Emily Mazor <emily.mazor@mail.utoronto.ca>
2019-12-01 20:56:55 -0500

    Added SubwayPlayer class to keep track of player stats. Implemented updatePlayerSubwayGameStats() in SubwayGame.endGame()

---
+++ phase2/UofTGame/app/src/main/java/com/group0611/uoftgame/games/subwaygame/SubwayPlayer.java
@@ -0,0 +1,26 @@
+package com.group0611.uoftgame.games.subwaygame;
+
+public class SubwayPlayer {
+//    SubwayGame subwayGame;
+    int coins;
+    int obstacles;
+    int score;
+
+    public SubwayPlayer() {
+        this.coins = 0;
+        this.obstacles = 0;
+        this.score = 10;
+    }
+
+    void increaseCoinsCollected() {
+        this.coins += 1;
+    }
+
+    void increaseObstacleCollisions() {
+        this.obstacles += 1;
+    }
+
+    void increaseTotalScore(int change) {
+        this.score += change;
+    }
+}
